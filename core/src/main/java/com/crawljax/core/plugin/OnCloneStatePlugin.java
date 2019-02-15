f74efd26593fcc18c1a19ac87b859c8e8007cbaa
林冠璋 <t107598020@ntut.org.tw>
2019-02-15 19:13:38 +0800

    Modify plugin and add some annotation

--- core/src/main/java/com/crawljax/core/plugin/OnCloneStatePlugin.java
+++ core/src/main/java/com/crawljax/core/plugin/OnCloneStatePlugin.java
@@ -1,10 +1,30 @@
 package com.crawljax.core.plugin;
 
+import com.crawljax.core.CandidateElement;
 import com.crawljax.core.CrawlerContext;
-import com.crawljax.core.state.StateFlowGraph;
 import com.crawljax.core.state.StateVertex;
-//ch-sh begin
+import com.google.common.collect.ImmutableList;
+
+
+/**
+ * When Crwaling State is a `Clone State` or `Same State`, will run this plugin
+ */
+
 public interface OnCloneStatePlugin extends Plugin {
-	void onCloneState(CrawlerContext context, StateVertex newState);
+
+	/**
+	 *  This will gave you all the action on the page, you can reconstruct the order
+	 * 		because the cache does not refresh the order, so I (TonyLin) add this plugin
+	 * 	
+	 * 	It's just like the {@link PreStateCrawlingPlugin}
+	 * 
+	 * @param context
+	 * 			the current session data.
+	 * @param candidateElements
+	 * 			the candidates for the current state.
+	 * @param newState
+	 * 			The state being crawled
+	 */
+
+	void onCloneState(CrawlerContext context, ImmutableList<CandidateElement> candidateElements, StateVertex newState);
 }
-//ch-sh end
