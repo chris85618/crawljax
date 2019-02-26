95ea15e22c1f394e4ed4d433edadbdb103603e3a
林冠璋 <t107598020@ntut.org.tw>
2019-02-26 22:32:51 +0800

    remove interface (maybe over design)

--- irobot-plugin/src/main/java/ntut/edu/tw/irobot/CrawlingData.java
+++ irobot-plugin/src/main/java/ntut/edu/tw/irobot/CrawlingData.java
@@ -1,4 +1,4 @@
-package ntut.edu.tw.irobot.interaction;
+package ntut.edu.tw.irobot.crawldata;
 
 
 import java.util.ArrayList;
@@ -16,8 +16,8 @@ import ntut.edu.tw.irobot.state.iRobotState;
 import ntut.edu.tw.irobot.state.State;
 
 
-public class Interactor implements CrawlerInteractor, RobotInteractor{
-    private static final Logger LOGGER = LoggerFactory.getLogger(Interactor.class);
+public class CrawlingData implements Cloneable{
+    private static final Logger LOGGER = LoggerFactory.getLogger(CrawlingData.class);
 
     private State currentState;
     private ImmutableList<Action> actions;
@@ -29,7 +29,7 @@ public class Interactor implements CrawlerInteractor, RobotInteractor{
     private String targetValue;
 
 
-    public Interactor() {
+    public CrawlingData() {
         this.currentState = null;
         this.targetAction = null;
         this.targetValue = "";
@@ -39,7 +39,6 @@ public class Interactor implements CrawlerInteractor, RobotInteractor{
         this.actions = ImmutableList.of();
     }
 
-    @Override
     public void convertToRobotAction (ImmutableList<CandidateElement> candidateElements) {
         LOGGER.info("Convert CadidateElements to iRobotActions...");
         List<Action> transformActions = new ArrayList<Action>();
@@ -58,7 +57,6 @@ public class Interactor implements CrawlerInteractor, RobotInteractor{
      * @return actions
      *              All actions from current page
      */
-    @Override
     public ImmutableList<Action> getActions() {
         LOGGER.info("Get the Action...");
         return actions;
@@ -70,7 +68,6 @@ public class Interactor implements CrawlerInteractor, RobotInteractor{
      * @param state
      *              The current State
      */
-    @Override
     public void convertToRobotState (StateVertex state) {
         LOGGER.info("Convert StateVertex to iRobotState...");
         currentState = new iRobotState(state);
@@ -79,7 +76,6 @@ public class Interactor implements CrawlerInteractor, RobotInteractor{
     /**
      * @return the currentState
      */
-    @Override
     public State getState() {
         LOGGER.info("Get the State...");
         return currentState;
@@ -91,17 +87,16 @@ public class Interactor implements CrawlerInteractor, RobotInteractor{
      * @param action
      *          The target action which iRobot assigned
      */
-    @Override
     public void setTargetAction(Action action, String value) {
         LOGGER.info("Get the target Action({}) from robot, transform it... ", action);
         targetAction = (CandidateElement) action.getSource();
+        targetValue = value;
     }
 
     /**
      * @return targetAction
      *          The target action which has been transfer to {@link com.crawljax.core.CandidateElement}
      */
-    @Override
     public CandidateElement getTargetElment() {
         LOGGER.info("Get the target element...");
         return targetAction;
@@ -111,19 +106,16 @@ public class Interactor implements CrawlerInteractor, RobotInteractor{
      * @param restartSignal
      *          The restart signal which iRobot gave.
      */
-    @Override
     public void setRestartSignal(boolean restartSignal) {
         LOGGER.info("Setting the restart signal : {}", restartSignal);
         this.restartSignal = restartSignal;
     }
 
-    @Override
     public void setTerminateSignal(boolean terminateSignal) {
         LOGGER.info("Setting the terminate signal : {}", terminateSignal);
         this.terminate = terminateSignal;
     }
 
-    @Override
     public boolean isExecuteSuccess() {
         LOGGER.info("Get the execute failure signal : {}", executeActionFailure);
         return executeActionFailure;
@@ -132,41 +124,41 @@ public class Interactor implements CrawlerInteractor, RobotInteractor{
     /**
      * @return the restart signal.
      */
-    @Override
     public boolean isRestart() {
         LOGGER.info("Get the restart signal : {}", restartSignal);
         return restartSignal;
     }
 
-    @Override
     public String getTargetElementType() {
         LOGGER.info("Get the target element type : {}", targetAction.getElement().getTagName());
         return targetAction.getElement().getTagName();
     }
 
-    @Override
     public String getTargetXpath() {
         LOGGER.info("Get the target element xpath : {}", targetAction.getIdentification().getValue());
         return targetAction.getIdentification().getValue();
     }
 
-    @Override
     public String getTargetValue() {
         LOGGER.info("Get the target value : {}", targetValue);
         return targetValue;
     }
 
-    @Override
-    public void setToDefaultSignal() {
-        LOGGER.info("Setting the signal to default...");
-        restartSignal = false;
+    public void setExecuteActionSignal(boolean successOrNot) {
+        LOGGER.info("Setting the execute signal : {}", successOrNot);
+        executeActionFailure = successOrNot;
+    }
+
+    public void resetData() {
+        LOGGER.info("Resetting data....");
         terminate = false;
+        restartSignal = false;
+        executeActionFailure = false;
+
     }
 
     @Override
-    public void setExecuteActionFailureSignal(boolean failSignal) {
-        LOGGER.info("Setting the execute failure signal : {}", failSignal);
-        executeActionFailure = failSignal;
+    public Object clone() throws CloneNotSupportedException{
+        return super.clone();
     }
-
 }
