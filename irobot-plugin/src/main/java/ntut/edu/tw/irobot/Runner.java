ba1e64edb4d654b65f63c2e842a8b8622f1edcb0
TonyLin <tony910124@gmail.com>
2019-09-09 17:01:03 +0800

    Fix the problem of multiple thread

--- irobot-plugin/src/main/java/ntut/edu/tw/irobot/Runner.java
+++ irobot-plugin/src/main/java/ntut/edu/tw/irobot/Runner.java
@@ -18,29 +18,36 @@ public class Runner {
 		/**
 		 *  Create the sever to communicate with  iRobot
 		 */
-		String java_port = "";
-		String python_port = "";
-		for (int i = 0; i < args.length; i+=2) {
-			if (args[i].equalsIgnoreCase("-java_port"))
-				java_port = args[i + 1];
-			if (args[i].equalsIgnoreCase("-python_port"))
-				python_port = args[i + 1];
-		}
-
-		try {
-			GatewayServer server = new GatewayServer.GatewayServerBuilder(new RobotServer())
-					.javaPort(Integer.parseInt(java_port))
-					.javaAddress(InetAddress.getByName("127.0.0.1"))
-					.callbackClient(Integer.parseInt(python_port), InetAddress.getByName("127.0.0.1"))
-					.build();
+		System.out.println(args.length);
+		if (args.length == 0) {
+			GatewayServer server = new GatewayServer(new RobotServer());
 			server.start();
-		} catch (UnknownHostException e) {
-			System.out.println("Can Not Find Host Name");
-			throw new RuntimeException("Can not find host name");
-		} catch (Exception e) {
-			e.printStackTrace();
-			throw new RuntimeException("Something went wrong...");
 		}
+		else {
+			String java_port = "";
+			String python_port = "";
 
+			for (int i = 0; i < args.length; i+=2) {
+				if (args[i].equalsIgnoreCase("-java_port"))
+					java_port = args[i + 1];
+				if (args[i].equalsIgnoreCase("-python_port"))
+					python_port = args[i + 1];
+			}
+
+			try {
+				GatewayServer server = new GatewayServer.GatewayServerBuilder(new RobotServer())
+						.javaPort(Integer.parseInt(java_port))
+						.javaAddress(InetAddress.getByName("127.0.0.1"))
+						.callbackClient(Integer.parseInt(python_port), InetAddress.getByName("127.0.0.1"))
+						.build();
+				server.start();
+			} catch (UnknownHostException e) {
+				System.out.println("Can Not Find Host Name");
+				throw new RuntimeException("Can not find host name");
+			} catch (Exception e) {
+				e.printStackTrace();
+				throw new RuntimeException("Something went wrong...");
+			}
+		}
 	}
 }
